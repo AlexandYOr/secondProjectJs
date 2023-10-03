96cde858c43486ed485fc884d3f583f486b60034
AlexandYOr <avo01121997@yandex.ru>
2023-10-03 20:57:55 +0300

    config webpack | lesson18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import oneFunc from "./modules/one";
 import twoFunc from "./modules/two";
-
+import css from "./css/style.css";
 
 oneFunc();
 twoFunc();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+            favicon: path.resolve(__dirname, 'public', 'favicon.png'),
+        })
+    ],
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    devtool: 'source-map',
+    devServer: {
+        hot: true,
+        port: 3000,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["css-loader"],
+            },
+        ],
+    }
+}
